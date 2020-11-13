5316ada55168859c52e892491d53bc402525aeb8
Santiago Fleitas <69471715+Sans-arch@users.noreply.github.com>
2020-11-13 14:04:26 -0300

    Atualizando os arquivos

--- frontend/src/usuario/telaUsuario.jsx
+++ frontend/src/usuario/telaUsuario.jsx
@@ -1,25 +1,48 @@
 import './Usuario.css'
 import React, {Component} from 'react'
 import { connect } from 'react-redux'
+import '../main/rotas'
 
 class usuario extends Component {   
     constructor(props){
         super(props)
         
         this.state={
-            nome:''
+            nome:'',
+            currentTime: 0
         }
     }
+    /*
+    tick(){
+        this.setState((prevState, props) => ({
+            currentTime: prevState.currentTime + 1
+        }))
+    }
+
+    componentDidMount(){
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+            )        
+    }
+*/
     
     
     render(){
+       /* 
+        if(this.state.currentTime === 6){
+            window.location.href='#/sair'
+        }*/
+
         if(this.props.nome !== ''){
             this.state.nome = this.props.nome
         }
+        
         console.log(this.props)
         return(
+            
             <div className="container">
-                <h1 className="titulo">Autenticado com sucesso!</h1>
+                <h1 className="titulo">Autenticado(a) com sucesso, {this.state.nome}!</h1>
                     <p>Nome: {this.state.nome}</p>
                     <p>Email: {this.props.email}</p>
                     <p>Telefone: {this.props.telefone}</p>
